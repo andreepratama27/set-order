8cbc2288ca994ea2fdd9b370f53e87a58a6621ae
Andre Pratama <andreepratama27@gmail.com>
2019-07-01 00:23:49 +0800

    add icons

--- android/app/src/main/java/com/setorder/MainApplication.java
+++ android/app/src/main/java/com/setorder/MainApplication.java
@@ -3,6 +3,7 @@ package com.setorder;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.swmansion.reanimated.ReanimatedPackage;
 import cl.json.RNSharePackage;
 import com.BV.LinearGradient.LinearGradientPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(new MainReactPackage(),
+            new VectorIconsPackage(),
             new ReanimatedPackage(), new RNSharePackage(), new LinearGradientPackage(),
           new RNGestureHandlerPackage());
     }
